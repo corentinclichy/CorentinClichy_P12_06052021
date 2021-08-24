46e1eab062cb24b9bead2636bc6d2a6526be6da3
Corentin Clichy <cor.clichy@gmail.com>
2021-08-23 20:28:27 -0400

    review spacing beetweend each components

--- src/components/commons/Content.js
+++ src/components/commons/Content.js
@@ -6,17 +6,17 @@ import Completion from './Completion';
 
 function Content() {
   return (
-    <main className="flex-1 p-7 pl-20">
-      <div className="header">
-        <h1 className="text-4xl">
+    <main className="flex-1 p-7 pl-20 pr-0">
+      <div className="header mb-7">
+        <h1 className="text-4xl ">
           Bonjour <span className="text-red-500">Thomas</span>
         </h1>
         <p className="mt-3">
           Félicitation ! Vous avez explosé vos objectifs hier 👏
         </p>
       </div>
-      <section className="user-stats flex gap-10 mt-5">
-        <div className="left-wrapper flex-1 flex flex-col gap-5">
+      <section className="user-stats flex  gap-5">
+        <div className="left-wrapper flex-1 max-w-4xl flex flex-col gap-5">
           <div className="row">
             <div className="dailyActivities-container bg-gray-50 h-64 p-7 pb-3 rounded">
               <DailyActivities />
